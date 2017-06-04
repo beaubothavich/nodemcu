741d5ed2ed23cd6d434a648da7d079df7bc7a034
Bothavich Saranapanichkul <beau.bothavich@live.com>
2017-06-04 23:14:00 +0700

    added temperature sensor and POST request to DB

--- NodeMCU.ino
+++ NodeMCU.ino
@@ -1,12 +1,19 @@
-#include <ESP8266WiFi.h>;
-#include <ESP8266HTTPClient.h>;
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 2
 
 const char* MY_SSID = "Live with Walkman_874b";
 const char* MY_PWD =  "55555555";
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+WiFiClient client;
+HTTPClient http;
 
 void setup() {
   Serial.begin(9600);
-  
   Serial.print("Connecting to "+*MY_SSID);
   WiFi.begin(MY_SSID, MY_PWD);
   Serial.println("going into wl connect");
@@ -18,22 +25,26 @@ void setup() {
   Serial.println("");
   Serial.println("WIFI connected\n ");
   Serial.println("");
+  sensors.begin();
 }
 
 void loop() {
-      WiFiClient client;
-      HTTPClient http;
+      Serial.print(" Requesting temperatures...");
+      sensors.requestTemperatures();
+      Serial.println("DONE");
+      Serial.print("Temperature for Device 1 is: ");
+      float temp = sensors.getTempCByIndex(0);
+      Serial.print(temp);
+
+      char tempoutstr[25] = "temp=";
+      String token = "&token=65beb48699b1e32986c50ee01fc1f4d3a0343f133cfff657b584863035bb14d3";
+      dtostrf(temp, 4, 2, &tempoutstr[5]);
+           
       http.begin("http://meteor-workspace-beaurulez.c9users.io/api/temp");
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      http.POST("temp=25&token=65beb48699b1e32986c50ee01fc1f4d3a0343f133cfff657b584863035bb14d3");
+      http.POST(tempoutstr+token);
       http.writeToStream(&Serial);
       Serial.println();
       http.end();
-  /*float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  Serial.print("temperature : ");
-  Serial.println(t);
-  Serial.print("humidity : ");
-  Serial.println(h);*/
   delay(3000);
 }
